f3e987e5e820d7bbc062ddb90d2881e8d653e719
NguyenDucDuc <nguyenducduc26797@gmail.com>
2023-02-10 17:18:46 +0700

    feature login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-
-import './App.css';
+import "./App.scss"
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './components/login/Login';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
